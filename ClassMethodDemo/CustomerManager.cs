0b6bc8ac9b79e137f1ab638afdee0332aaba359e
kenanismayil <kenanismayil434@gmail.com>
2022-05-05 02:08:53 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine($"{customer.Name} {customer.Surname} Bankaya Eklendi.");
+        }
+
+        public void CustomerList(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname);
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine($"{customer.Name} {customer.Surname} Bankadan Silindi! ");
+        }
+
+        public void OperationEnd(string endMessage)
+        {
+            Console.WriteLine(endMessage);
+        }
+    }
+}
